c2274db93eeb7469576a52328b6b032e6b671a9e
张东旭 <575078943@qq.com>
2016-04-04 16:25:09 +0800

    no message

---
+++ hhhhhh/hhhhhh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hhhhhh
+//
+//  Created by 张东旭 on 16/4/4.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hhhhhh/hhhhhh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hhhhhh
+//
+//  Created by 张东旭 on 16/4/4.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
